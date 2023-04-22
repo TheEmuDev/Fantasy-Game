7f1aa331c9d97a958e42a64ce0d43754e7df2c40
CranePrograms <IanSullivanOTI@gmail.com>
2023-04-22 18:11:17 -0500

    Slime Detection WIP

--- Assets/Scripts/Enemies/SlimeEnemy.cs
+++ Assets/Scripts/Enemies/SlimeEnemy.cs
@@ -31,10 +31,26 @@ public class SlimeEnemy : Enemy
         }
     }
 
+    //It will always detect itself as being inside the detection layer since the detection layer is including the slime hitbox
+    //I need to basically check to see if the detection is including the slime instead of the detection layer probably
+    //Otherwise it works.
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        LayerMask mask = LayerMask.GetMask("Detection");
+        if(collision.IsTouchingLayers(mask))
+        {
+            Debug.Log("Touching Detection Layer!");
+        }
+
+
+
+        Debug.Log("Trigger Entered");
+
         if(collision.gameObject.CompareTag("Player") && targetedPlayer == null) {
             targetedPlayer = collision.gameObject;
+            Debug.Log("Slime collided with the player");
         }
     }
 
@@ -45,6 +61,7 @@ public class SlimeEnemy : Enemy
             if (collision.gameObject == targetedPlayer)
             {
                 targetedPlayer = null;
+                Debug.Log("Slime is done colliding with the player");
             }
         }
     }
